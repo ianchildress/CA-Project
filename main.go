590494fe7c2bc67d3b07f498be3db8222f215034
ianchildress <ianchildress@gmail.com>
2015-08-16 10:57:51 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	//"os/exec"
+	"github.com/fsouza/go-dockerclient"
+)
+
+func main() {
+	/*
+		cmd := exec.Command("docker", "ps")
+		out, err := cmd.Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		fmt.Print(string(out))
+	*/
+
+	endpoint := "unix:///var/run/docker.sock"
+	client, _ := docker.NewClient(endpoint)
+	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+	for _, img := range imgs {
+		fmt.Println("ID: ", img.ID)
+		fmt.Println("RepoTags: ", img.RepoTags)
+		fmt.Println("Created: ", img.Created)
+		fmt.Println("Size: ", img.Size)
+		fmt.Println("VirtualSize: ", img.VirtualSize)
+		fmt.Println("ParentId: ", img.ParentID)
+	}
+}
